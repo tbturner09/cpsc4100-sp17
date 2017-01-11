7a1152c01d7bf35112e656c174d48cd068cae721
Craig Tanis <craig-tanis@utc.edu>
2017-01-11 13:16:10 -0500

    hello world

---
+++ C/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>              /* preprocessor directive */
+
+int main(int argc, char *argv[]) /* program entry point with command line arguments */
+{
+    printf("Hello World\n");    /* a subroutine that does IO -- declared in stdio.h */
+    return 0;                   /* return code from main -> exit code for program */
+}
